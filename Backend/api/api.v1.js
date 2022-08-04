10b9f41967355ce0e50dc235f9d35783102e25fd
Aldaine Clarke <vainedev@gmail.com>
2022-08-04 08:22:55 -0400

    updated the api file with the correct location of routers

--- Backend/api/api.v1.js
+++ Backend/api/api.v1.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const patientsRouter = require("../routes/patients.routes");
-
+const usersRouter = require("../routes/users.routes");
+const recordsRouter = require("../routes/medrec.routes");
+const doctorsRouter = require("../routes/doctors.routes");
 router.use("/patients/",patientsRouter);
 router.use("/users/", usersRouter)
-router.use("/departments/", departmentsRouter)
+// router.use("/departments/", departmentsRouter)
 router.use("/records/", recordsRouter)
 router.use("/doctors/", doctorsRouter)
 
