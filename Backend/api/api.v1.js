335f13213aca534405d2f3e9bd921309768f87ce
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-22 22:07:12 -0500

    Updated the api file to ensure that the appointment routes are registered

--- Backend/api/api.v1.js
+++ Backend/api/api.v1.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const patientsRouter = require("../routes/patients.routes");
+const appointmentsRouter = require("../routes/appointments.routes");
 const usersRouter = require("../routes/users.routes");
 const recordsRouter = require("../routes/medrec.routes");
 
 router.use("/patients/",patientsRouter);
-router.use("/users/", usersRouter)
-router.use("/records/", recordsRouter)
+router.use("/users/", usersRouter);
+router.use("/records/", recordsRouter);
+router.use("/appointments/", appointmentsRouter)
 
 module.exports = router
