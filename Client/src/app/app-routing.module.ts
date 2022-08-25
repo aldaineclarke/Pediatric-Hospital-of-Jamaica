8c30b0ec21116ca00798ae1ecbe13c5cbd28e999
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-25 16:01:44 -0500

    created nav service to remove navbar if the route is admin route

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { AdminHomeComponent } from './Pages/admin-home/admin-home.component';
 import { AdminLoginComponent } from './Pages/admin-login/admin-login.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { NavService } from './Services/nav.service';
 
 const routes: Routes = [
   {
@@ -29,4 +30,16 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  constructor(private router:Router, private navService: NavService){
+    this.router.events.subscribe((res)=>{
+        if(location.href.indexOf("admin") > -1){
+          console.log(location.href.indexOf("admin"))
+          this.navService.removeOriginalNav();
+        }else if(location.href.indexOf("admin") == -1 ){
+          console.log("admin")
+          this.navService.addOriginalNav();
+        }
+    })
+  }
+}
