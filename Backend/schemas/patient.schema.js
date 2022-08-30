9149ec8aa7f63ddff5b79d5b094f4df59c99339b
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-30 11:00:45 -0500

    updated the schema for patient

--- Backend/schemas/patient.schema.js
+++ Backend/schemas/patient.schema.js
@@ -3,15 +3,24 @@ const { Schema, model } = require("mongoose");
 
 let patientSchema = new Schema({
     fname: {type:String},
-    lname: {type:String},
-    mname:{type:String},
-    email: {type:String},
-    DOB: {type:Date},
+    lname: {type: String},
+    mname:{type: String},
+    email: {type: String},
+    DOB: {type: Date},
     patientImage: {type:String},
-    guardianName:{type: Schema.Types.ObjectId, ref: "User"},
-    gender:{type: String},
+    guardian_fname:{type:String},
+    guardian_lname:{type:String},
+    guardian_address:[{
+        street: {type:String},
+        city:{type:String},
+        parish: {type:String}
+    }],
+    phone: {type:String},
+    gender:{type:String},
     allergies: {type:String},
     nationality:{type:String},
+    admission_date:{type: String},
+    admit_doctor: {type:String},
 
 }, {timestamps:true});
 // Will create timestamps for the createdAt and updatedAt time;
