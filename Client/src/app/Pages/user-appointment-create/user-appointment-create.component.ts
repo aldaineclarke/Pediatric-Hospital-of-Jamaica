9326791863cb5eeec2fdca36d6ce352d1b63c149
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-02 11:50:32 -0500

    created method to managed the data submitted from the form

--- Client/src/app/Pages/user-appointment-create/user-appointment-create.component.ts
+++ Client/src/app/Pages/user-appointment-create/user-appointment-create.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/Services/auth.service';
+import { AppointmentService } from 'src/app/Services/appointment.service';
+import { Router } from '@angular/router';
+import { User } from 'src/app/Interfaces/user';
+import { UsersService } from 'src/app/Services/users.service';
 @Component({
   selector: 'hos-user-appointment-create',
   templateUrl: './user-appointment-create.component.html',
@@ -8,14 +13,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class UserAppointmentCreateComponent implements OnInit {
 
-  constructor(private location: Location) { }
-
+  constructor(private location: Location, private authService: AuthService, private appointmentService: AppointmentService, private router:Router, private usersService: UsersService) { }
+  user = this.authService.getUser();
+  doctors:User[] = []
   ngOnInit(): void {
+    console.log( this.authService.getUser())
+    this.getAllDoctors()
   }
 
   appointmentForm = new FormGroup({
     fname: new FormControl('', Validators.required),
     lname: new FormControl('', Validators.required),
+    title: new FormControl('', Validators.required),
     gender: new FormControl('Male', Validators.required),
     email: new FormControl('', Validators.required),
     phone: new FormControl(''),
@@ -29,4 +38,20 @@ export class UserAppointmentCreateComponent implements OnInit {
     this.location.back();
   }
 
+  submitForm(){
+    let data = this.appointmentForm.value;
+    data.userId = this.user._id;
+    console.log(data);
+    this.appointmentService.createAppointment(data).subscribe(()=>{
+      this.router.navigate(["/user/appointments"])
+    })
+  }
+
+  getAllDoctors(){
+    this.usersService.getAllUsers("Doctor").subscribe((response)=>{
+      this.doctors = response.data;
+      console.log(this.doctors);
+
+    })
+  }
 }
