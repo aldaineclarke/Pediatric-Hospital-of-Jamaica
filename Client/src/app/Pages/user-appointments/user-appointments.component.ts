bb3fa812c88609b14d7c53dd60834ae3f22efb18
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-02 11:49:44 -0500

    added method to retrieve all appointment that matches an id passed

--- Client/src/app/Pages/user-appointments/user-appointments.component.ts
+++ Client/src/app/Pages/user-appointments/user-appointments.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment, AppointmentPop } from 'src/app/Interfaces/appointment';
+import { AppointmentService } from 'src/app/Services/appointment.service';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'hos-user-appointments',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAppointmentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private appointmentService: AppointmentService, private authService: AuthService) { }
+  userAppointments:AppointmentPop[] = [];
+  userData = this.authService.getUser()
+
 
   ngOnInit(): void {
+    console.log(this.authService.getUser())
+    this.getAllAppointments();
+  }
+  getAllAppointments(){
+    this.appointmentService.getUserAppointments(this.userData._id).subscribe((response)=>{
+      this.userAppointments = response.data
+      console.log(this.userAppointments);
+    })
   }
 
 }
