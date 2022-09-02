82f8acae69c1389c35502890f30f13249f3ffab5
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-02 18:25:15 -0500

    retrieved the user from the login information

--- Client/src/app/Pages/user-wrapper/user-wrapper.component.ts
+++ Client/src/app/Pages/user-wrapper/user-wrapper.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from 'src/app/Interfaces/user';
+import { AuthService } from 'src/app/Services/auth.service';
+import { UsersService } from 'src/app/Services/users.service';
 
 @Component({
   selector: 'hos-user-wrapper',
@@ -8,15 +11,25 @@ import { Router } from '@angular/router';
 })
 export class UserWrapperComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private authService: AuthService, private usersService: UsersService) { }
 
   ngOnInit(): void {
+    this.getUserInfo();
   }
 
+  user!: User;
   navOpened: boolean = false
 
   toggleNav(){
     this.navOpened = !this.navOpened;
+  }
+
+  getUserInfo(){
+    const id = this.authService.getUser()._id;
+    console.log(id);
+    this.usersService.getUserById(id).subscribe((response)=>{
+        this.user = response.data;
+    })
   }
 
   logoutUser(){
