5c11b79dc4b48cda04da4ac8d0f615f1c86838a0
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-01 23:03:57 -0500

    created profile page

--- Client/src/app/Pages/user-profile/user-profile.component.ts
+++ Client/src/app/Pages/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'hos-user-profile',
@@ -7,7 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location) { }
+
+  updateProfileForm = new FormGroup({
+    fname: new FormControl("", Validators.required),
+    lname: new FormControl("", Validators.required),
+    username: new FormControl("", Validators.required),
+    email: new FormControl("", Validators.required),
+    phone: new FormControl("", Validators.required),
+    street: new FormControl("", Validators.required),
+    city: new FormControl("", Validators.required),
+    parish: new FormControl("", Validators.required),
+    password: new FormControl("", Validators.required),
+
+  });
+
+  goBack(){
+    this.location.back();
+  }
 
   ngOnInit(): void {
   }
