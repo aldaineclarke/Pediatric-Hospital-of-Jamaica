85a6e5a3d9008240a8c6cb9c33a46ce958d6d6da
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-03 10:33:40 -0500

    Updated user schema and created a separate schema for doctors

---
+++ Backend/schemas/doctor.schema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+
+const doctorSchema = mongoose.Schema({
+    fname: {type:String, required:true},
+    lname: {type:String, required:true},
+    title:{type: String},
+    gender:{type: String},
+    imageUrl: {type: String},
+    email:{type:String, unique:true, lowercase:true},
+    phone: {type:String, required:true},
+    department:{
+        type:String, 
+        required:[true, "Doctor must be apart of a department"], 
+        enum:{
+            values:["SURGEON", "DENTISTRY", "RADIOLOGY","CARDIOLOGY", "DERMATOLOGY","OPTHALMOLOGY", "NEUROLOGY", "EMERGENCY" ],
+            message: '{VALUE} is not a valid department, valid departments are: ["SURGEON", "DENTISTRY", "RADIOLOGY","CARDIOLOGY", "DERMATOLOGY","OPTHALMOLOGY", "NEUROLOGY", "EMERGENCY" ]'
+        }
+    },
+    username: {type: String, unique: true, required: true},
+    address:{
+        street: {type: String},
+        city:{type: String},
+        parish:{type: String},
+    },
+    patientCount:{type:Number, default:0},
+    experience: {type: Number, default: 5, min:[5, "You should have atleast 5 years of experience"]},
+    status: {
+        type: String, 
+        enum:{
+            values: ["AVAILABLE", "IN MEETING", "BUSY", "UNAVAILABLE"], 
+            message: "{VALUE} should be 'AVAILABLE','IN MEETING','BUSY','UNAVAILABLE'"
+            },
+        default: "AVAILABLE"},
+    password: {type:String, required:[true,"password field should not be empty"]},
+    rating: {type:Number, default: 5},
+    isSuperAdmin: {type:Boolean, default:false}
+
+}, {timestamps:true});
+
+module.exports = mongoose.model('Doctor',doctorSchema);
--- Backend/schemas/user.schema.js
+++ Backend/schemas/user.schema.js
@@ -5,12 +5,9 @@ const userSchema = mongoose.Schema({
     fname: {type:String, required:true},
     lname: {type:String, required:true},
     title:{type: String},
-    gender:{type: String},
     imageUrl: {type: String},
     email:{type:String, unique:true, lowercase:true},
     phone: {type:String, required:true},
-    role:{type:String, required:true, default:"Customer"},
-    department:{type:String, required:true, default: "Customer"},
     username: {type: String, unique: true, required: true},
     address:{
         street: {type: String},
@@ -18,7 +15,6 @@ const userSchema = mongoose.Schema({
         parish:{type: String},
     },
     password: {type:String, required:true},
-    isSuperAdmin: {type:Boolean, default:false}
 
 }, {timestamps:true});
 
