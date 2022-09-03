dd48a94a9bb721249e7cb6ab64fceac57a8c8e01
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-03 05:07:32 -0500

    updated card component to retrieve the appointment object as input

--- Client/src/app/Components/appointment-card/appointment-card.component.ts
+++ Client/src/app/Components/appointment-card/appointment-card.component.ts
@@ -1,21 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { AppointmentPop } from 'src/app/Interfaces/appointment';
 
 @Component({
   selector: 'hos-appointment-card',
   templateUrl: './appointment-card.component.html',
   styleUrls: ['./appointment-card.component.scss']
 })
-export class AppointmentCardComponent implements OnInit {
+export class AppointmentCardComponent implements OnInit, OnChanges {
 
-  @Input() dataPresent!: boolean;
-  month_day = "Aug 22";
-  time = "10: 20AM"
-  doctor = "Dr. Warren";
-  specialty = "Dentist";
+  @Input("appointment") appointment!: AppointmentPop;
+  month_day = ""
+  time = " "
+  doctor = ""
+  specialty = ""
   imageSrc = "/assets/mother-two-her-sons-have-appointment-with-pediatrician.jpg";
   constructor() { }
 
   ngOnInit(): void {
+  }
+  ngOnChanges(){
+    this.month_day = this.appointment.visitStart.toDateString().slice(4,11);
+    this.time = this.appointment.visitStart.toLocaleTimeString("en-US", {timeStyle:"short"});
+    this.doctor = this.appointment.doctor.title+ " "+ this.appointment.doctor.lname;
+    this.specialty = this.appointment.doctor.department as string;
   }
 
 }
