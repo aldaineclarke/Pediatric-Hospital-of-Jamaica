9595e0b67353dea7b6cdae4bb05402c90ca1355a
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-03 11:18:12 -0500

    updated api to add doctors routes

--- Backend/api/api.v1.js
+++ Backend/api/api.v1.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const patientsRouter = require("../routes/patients.routes");
 const appointmentsRouter = require("../routes/appointments.routes");
 const usersRouter = require("../routes/users.routes");
+const doctorsRouter = require("../routes/doctors.routes");
 const recordsRouter = require("../routes/medrec.routes");
 
 router.use("/patients/",patientsRouter);
 router.use("/users/", usersRouter);
+router.use("/doctors/", doctorsRouter);
 router.use("/records/", recordsRouter);
 router.use("/appointments/", appointmentsRouter)
 
