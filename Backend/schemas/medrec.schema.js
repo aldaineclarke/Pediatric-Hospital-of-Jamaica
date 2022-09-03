5e38f96659e8fee217dce0794fcb228364c5254e
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-03 15:34:17 -0500

    updated reference for schema

--- Backend/schemas/medrec.schema.js
+++ Backend/schemas/medrec.schema.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 let medRecord = new Schema({
     patient: {type: Schema.Types.ObjectId, ref:"Patient"},
-    doctor: {type:Schema.Types.ObjectId, ref:"User"},
+    doctor: {type:Schema.Types.ObjectId, ref:"Doctor"},
     complaint:{type: String},
     diagnosis: {type: String},
     comments: [{
