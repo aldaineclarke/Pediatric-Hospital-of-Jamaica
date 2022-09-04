55a40b47b25885b6565209c4fcf938f9d5b5983d
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-03 22:00:45 -0500

    added another news api endpoint

--- Client/src/app/Services/vendor.service.ts
+++ Client/src/app/Services/vendor.service.ts
@@ -8,11 +8,13 @@ import { NewsApiResponse } from '../Interfaces/news';
 })
 export class VendorService {
 
-  private NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=7dfa680079a74b0ba1a6f9fd72ef36f8"
+  private NEWS_ENDPOINT = "https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=7dfa680079a74b0ba1a6f9fd72ef36f8";
+  
+  private NEWS_ENDPOINT2 = "https://saurav.tech/NewsAPI/top-headlines/category/health/us.json";
 
   constructor(private _http: HttpClient) { }
 
   getAllHealthNews():Observable<NewsApiResponse>{
-    return this._http.get<NewsApiResponse>(this.NEWS_ENDPOINT) 
+    return this._http.get<NewsApiResponse>(this.NEWS_ENDPOINT2) 
   }
 }
