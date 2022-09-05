58c7a611a0ce691908839999e08034870795a506
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-04 19:18:26 -0500

    started working on the news carousel

--- Client/src/app/Pages/user-home/user-home.component.ts
+++ Client/src/app/Pages/user-home/user-home.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentPop } from 'src/app/Interfaces/appointment';
+import { News } from 'src/app/Interfaces/news';
 import { User } from 'src/app/Interfaces/user';
 import { AppointmentService } from 'src/app/Services/appointment.service';
 import { AuthService } from 'src/app/Services/auth.service';
+import { VendorService } from 'src/app/Services/vendor.service';
 
 @Component({
   selector: 'hos-user-home',
@@ -11,11 +13,13 @@ import { AuthService } from 'src/app/Services/auth.service';
 })
 export class UserHomeComponent implements OnInit {
 
-  constructor(private authService: AuthService, private appointmentService: AppointmentService) { }
+  constructor(private authService: AuthService, private appointmentService: AppointmentService, private vendorService: VendorService) { }
   upAppointment!: AppointmentPop;
 
+  mainNews:News[] = [];
   ngOnInit(): void {
     this.getUserAppointment();
+    this.getNews();
   }
 
   getUserAppointment(){
@@ -27,6 +31,15 @@ export class UserHomeComponent implements OnInit {
         console.log(this.upAppointment);
       }
     })
+  }
+
+  getNews(){
+    this.vendorService.getAllHealthNews().subscribe((response)=>{
+      this.mainNews = response.articles.slice(5,10);
+    })
+  }
+  goToPage(route:string){
+    location.href=route
   }
 
 }
