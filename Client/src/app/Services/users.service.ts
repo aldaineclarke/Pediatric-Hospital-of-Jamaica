26fdf3955eebbdbcfc51baecabc4fd9cae95973d
aldaine-AG <aldaineclarke1@gmail.com>
2022-09-07 00:33:06 -0500

    Create handler for success and failed request using sweetalerts and created behavior subject for the user after login

--- Client/src/app/Services/users.service.ts
+++ Client/src/app/Services/users.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of, throwError, tap } from 'rxjs';
+import { catchError, Observable, of, throwError, tap, BehaviorSubject } from 'rxjs';
 import { API_Response, User } from '../Interfaces/user';
-import { NotificationService } from './notification.service';
 import Swal from 'sweetalert2';
 
 @Injectable({
@@ -10,50 +9,82 @@ import Swal from 'sweetalert2';
 })
 export class UsersService {
 
-  private  USERS_ENDPOINT = "http://localhost:3000/api/v1/users"
-  constructor(private _http: HttpClient, private notificationService: NotificationService) { }
+  private  USERS_ENDPOINT = "http://10.44.16.32:3000/api/v1/users";
 
-  private handleErrror(error:any) { 
+  private userSubject = new BehaviorSubject<User | null>(null);
+
+  public user$:Observable<User | null> = this.userSubject.asObservable();
+
+  createUserLoginSession(user:User){
+    this.userSubject.next(user);
+  }
+  removeUserLoginSession(){
+    this.userSubject.next(null);
+  }
+
+  constructor(private _http: HttpClient) { }
+
+  private handleErrror(error:any, status:string) { 
     let message = ""
       if(error.error.message){
         message = error.error.message;
-      }else message = error;
+        Swal.fire(
+          status,
+          message,
+          'error'
+        )
+      }else if(error.status == 0){
+        Swal.fire(status, error.statusText, "error");
+      }
     return throwError(()=> new Error(message));
+  };
+
+  private handleSuccess<T>(response:API_Response<T>){
+    if(response.status == "Success"){
+      Swal.fire(response.status, response.message, "success");
+    }else if(response.status == "Failed"){
+      Swal.fire(response.status, response.message, "error");
+    }
   }
+
 
   getAllUsers():Observable<API_Response<User[]>>{
     return this._http.get<API_Response<User[]>>(this.USERS_ENDPOINT)
   }
   getUserById(id:string):Observable<API_Response<User>>{
     return this._http.get<API_Response<User>>(this.USERS_ENDPOINT+ "/"+id)
+    .pipe(
+      // tap(this.handleSuccess),
+      catchError((error)=>this.handleErrror(error,"User not found"))
+    )
   }
   createUser(data: Partial<User>){
-    return this._http.post(this.USERS_ENDPOINT, data);
+    return this._http.post<API_Response<User>>(this.USERS_ENDPOINT, data)
+    .pipe(
+      tap(this.handleSuccess),
+      catchError((error)=> this.handleErrror(error, "Registration Failed"))
+    )
   }
   updateUser(id: string, changes: Partial<User>){
-    return this._http.patch(this.USERS_ENDPOINT+"/"+ id, changes);
+    return this._http.patch<API_Response<User>>(this.USERS_ENDPOINT+"/"+ id, changes)
+    .pipe(
+      tap(this.handleSuccess),
+      catchError((error)=> this.handleErrror(error, "Update Failed"))
+    );
   }
   deleteUser(id:string){
-    return this._http.delete(this.USERS_ENDPOINT+ "/"+id);
+    return this._http.delete(this.USERS_ENDPOINT+ "/"+id)
+
   }
 
   loginUser(data:Partial<User>){
-    return this._http.post<API_Response<string>>(this.USERS_ENDPOINT+"/login",data)
+    return this._http.post<API_Response<{user:User, token:string}>>(this.USERS_ENDPOINT+"/login",data)
     .pipe(
-      catchError((error)=>{
-        let message = error.error.message;
-        console.log(message);
-        Swal.fire(
-          'Authentication Failed!',
-          message,
-          'error'
-        )
-        return this.handleErrror(error);
-      }),
+      catchError((error)=>  this.handleErrror(error, "Authentication Failed")),
       tap((response)=>{
         if(response.data){
-
-          localStorage.setItem("token",response.data);
+          this.createUserLoginSession(response.data.user);
+          localStorage.setItem("token",response.data.token);
           Swal.fire(
             'Login Successful',
             'success'
